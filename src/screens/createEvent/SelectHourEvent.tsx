22f6b1050d490afbb8565d3a83246fd782d03746
Daniel Souza <danielfss98@gmail.com>
2019-05-05 11:17:46 -0300

    Altera nome da tela

--- src/screens/createEvent/SelectHourEvent.tsx
+++ src/screens/createEvent/SelectHourEvent.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View } from "react-native";
 
-export default class SelectHourMatch extends Component {
+export default class SelectHourEvent extends Component {
   render() {
     return (
       <View>
